5bc76e24b47c6e08c289fa36ac4ab00fb8d614e4
Christopher Kevin Costa <christopherkevin78@gmail.com>
2024-02-24 16:03:36 -0300

    Atualização de CRUD
    Adição de funções de editar e deletar.

--- Projeto_cad_usuarios/Projeto_cad_usuarios/urls.py
+++ Projeto_cad_usuarios/Projeto_cad_usuarios/urls.py
@@ -5,5 +5,9 @@ from app_cad_usuarios import views
 urlpatterns = [
     #Rota, view responsável, nomede referência
     path('',views.home, name='home'),
-    path('usuarios/', views.usuarios, name= 'listagem_usuarios')
+    path('usuarios/', views.usuarios, name= 'listagem_usuarios'),
+    path('editar/<int:id_usuario>/', views.editar, name= 'editar'),
+    path('update/<int:id_usuario>/', views.update, name= 'update'),
+    path('excluir/<int:id_usuario>/', views.excluir_item, name= 'excluir_item'),
+    path('read/', views.read, name='read'),
 ]
--- Projeto_cad_usuarios/app_cad_usuarios/views.py
+++ Projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from .models import Usuario
 
 def home(request):
     return render(request,'usuarios/home.html')
 
+def read(request):
+    return redirect('listagem_usuarios')
+
 def usuarios(request):
-    #Salvar as informações no banco de dados
-    novo_usuario = Usuario()
-    novo_usuario.nome = request.POST.get('nome')
-    novo_usuario.idade = request.POST.get('idade')
-    novo_usuario.email = request.POST.get('email')
-    novo_usuario.telefone = request.POST.get('telefone')
-    novo_usuario.save()
+    
+    if request.method == 'POST':
+        #Salvar as informações no banco de dados
+        novo_usuario = Usuario()
+        novo_usuario.nome = request.POST.get('nome')
+        novo_usuario.idade = request.POST.get('idade')
+        novo_usuario.email = request.POST.get('email')
+        novo_usuario.telefone = request.POST.get('telefone')
+        novo_usuario.save()
     
     #Exibir todos os usuarios já cadastrados em uma nova pagina
     usuarios = {
@@ -20,3 +26,24 @@ def usuarios(request):
     
     #Retorna os dados para a pagina de listagem de usuarios
     return render(request, 'usuarios/usuarios.html', usuarios)
+
+def editar(request, id_usuario):
+    usuario = Usuario.objects.get(id_usuario=id_usuario)
+    return render(request, 'usuarios/editar.html', {"usuario": usuario})
+
+def update(request, id_usuario):
+    usuario = Usuario.objects.get(id_usuario=id_usuario)
+    usuario.nome = request.POST.get('nome')
+    usuario.idade = request.POST.get('idade')
+    usuario.email = request.POST.get('email')
+    usuario.telefone = request.POST.get('telefone')
+    usuario.save()
+    return redirect(read)
+
+def excluir_item(request, id_usuario):
+    if request.method == 'POST':
+        usuario = Usuario.objects.get(pk=id_usuario)
+        usuario.delete()
+        return render(request, 'usuarios/excluir.html')
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Método inválido'})
